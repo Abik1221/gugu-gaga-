9a7c8285fbd535da69efd6a840721ff29d5ee8d9
unknown <nahomkeneni4@gmail.com>
2025-11-10 16:12:29 +0300

    ssl certeficate automation

--- front_end/next.config.ts
+++ front_end/next.config.ts
@@ -11,6 +11,9 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
