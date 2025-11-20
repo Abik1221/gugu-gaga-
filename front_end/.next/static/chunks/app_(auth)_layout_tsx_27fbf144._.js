19e908ebf3d32e2cafd90e813bc991c0bd2600ba
unknown <nahomkeneni4@gmail.com>
2025-11-20 13:44:38 +0300

    n

---
+++ front_end/.next/static/chunks/app_(auth)_layout_tsx_27fbf144._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_3fd5d0ae._.js",
+  "static/chunks/_137a3701._.js"
+],
+    source: "dynamic"
+});
